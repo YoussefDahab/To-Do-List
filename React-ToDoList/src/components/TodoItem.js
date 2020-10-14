f5a163f290699c8ece5facef158435573d865fe0
YoussefDahab <57932145+YoussefDahab@users.noreply.github.com>
2020-10-14 00:07:34 -0400

    Update TodoItem.js

--- React-ToDoList/src/components/TodoItem.js
+++ React-ToDoList/src/components/TodoItem.js
@@ -14,14 +14,12 @@ export class TodoItem extends Component {
 
     
     render() {
-        //Destructuring allows us to pull variables out of lists. Instead of doing this.props.todo.id now I just say id. Same with title
         const { id, title } = this.props.todo;
         return (
             <div style = { this.getStyle() }>
                 <p>
-                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {/*To make the checkbox do something we add the event OnChange.*/}
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> 
                     { title }
-                    {/*We add an event to this button so when we click it, the TodoItem is deleted*/}
                     <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
                 </p>
             </div>
@@ -39,26 +37,10 @@ const btnStyle = {
     background: '#ff0000',
     color: '#fff',
     border: 'none',
-    padding: '5px 9px',    //5px top and bottom while 10px left and right
-    borderRadius: '50%',      // rounded button
+    padding: '5px 9px', 
+    borderRadius: '50%',      
     cursor: 'pointer',
     float: 'right'
 }
 
 export default TodoItem
-
-
-
-
-/* 1. We can use inline style (using double curly braces {{}}. It is identical to CSS but we do camel case instead of hyphens: backgroundColor not background-color 
-<div style = {{ backgroundColor: '#f4f4f4'}} ) 
-in JSX which allows us to have our markup, functionality, and styling all right in our component
-
-2. we can also use a variable.
-const itemStyle = {
-    backgroundColor: '#f4f4f4'
-}
-then <div style = { itemStyle }>
-
-3. style can also be put inside of a function because we want the style to change depending on if the todo has been completed or not
-*/
