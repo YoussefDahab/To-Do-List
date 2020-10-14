42f8052818db1e81661b1bd847ccbc3a44974f6a
YoussefDahab <57932145+YoussefDahab@users.noreply.github.com>
2020-10-14 00:06:38 -0400

    Update AddTodo.js

--- React-ToDoList/src/components/AddTodo.js
+++ React-ToDoList/src/components/AddTodo.js
@@ -1,8 +1,3 @@
-// class based component - type rce then hit tab
-/*1. When we have an input field, usually you would want to have your fields as a piece of state for that component. 
-2. As you type in the input field, an onChange event will be fired off and you need to update the state. If I type hello in the input field, then the title in the 
-state should be updated to hello. Therefore, the input field has a value of this.state.title and an onChange event*/
-
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
@@ -11,13 +6,12 @@ export class AddTodo extends Component {
         title: ''
     }
 
-    // Its going to try to submit to the actual file. We can stop that by e.preventDefault()
     onSubmit = (e) => {
         e.preventDefault();
         this.props.addTodo(this.state.title);
         this.setState({ title: ''});
     }
-    //event handlers take in event parameter e - e.target.value returns whatever we type in the input field
+    
     onChange = (e) => this.setState({ [e.target.name]: e.target.value })
 
     render() {
